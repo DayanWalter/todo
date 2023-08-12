4338a10c8d858d5be73b576ad0204edcc5e15c6d
SynthCyraX <SynthCyraX@gmail.com>
2023-08-12 15:07:54 +0200

    Copy test done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import "./styles/main.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import Task from "./modules/Task";
-import Project from "./modules/Project";
 import Storage from "./modules/Storage";
 import UI from "./modules/UI";
 
@@ -13,6 +11,6 @@ const addProjectButton = document.getElementById("addProjectButton");
 addProjectButton.addEventListener("click", Storage.addProject);
 
 const projectContainer = document.getElementById("projectContainer");
-
 projectContainer.addEventListener("click", Storage.deleteProject);
+
 // document.addEventListener("DOMContentLoaded", UI.loadHomepage);
